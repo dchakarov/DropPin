8d38abccfda12b080c358a74464e093f2644fe9d
Dimitar Chakarov <dimitar@dchakarov.com>
2020-09-19 18:51:27 +0100

    Initial Commit

---
+++ DropPinDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DropPinDemo
+//
+//  Created by Dimitar Chakarov on 19/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DropPinDemo/DropPinDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropPinDemoApp.swift
+//  DropPinDemo
+//
+//  Created by Dimitar Chakarov on 19/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct DropPinDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
